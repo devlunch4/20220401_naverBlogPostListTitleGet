cbd1710143e0f9932bc20eb04791eae06d20c1a6
devlunch4@E470 <devlunch4@gmail.com>
2022-06-30 02:01:30 +0900

    added. datatiem.now()

--- calendar_text.py
+++ calendar_text.py
@@ -5,3 +5,9 @@ year = datetime.datetime.now().year
 cal = calendar.TextCalendar(calendar.SUNDAY)
 for m in range(1, 13):
     print(cal.formatmonth(year, m, 0, 0))
+
+###
+
+from datetime import datetime
+now = datetime.now()
+print(now.strftime("%d-%b-%Y %H:%M:%S"))
